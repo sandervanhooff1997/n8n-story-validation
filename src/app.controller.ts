85e4334d503c35dbef29e6ac964a8394dffe0700
Sander van Hooff <s.vanhooff@hotmail.com>
2025-10-30 14:36:22 +0100

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     const numA = parseFloat(a);
     const numB = parseFloat(b);
 
-    // Validate inputsss
+    // Validate inputs
     if (isNaN(numA) || isNaN(numB)) {
       return 'Invalid numbers provided.';
     }

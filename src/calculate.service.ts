09ffc4f0bc8da3cfe60f48f764d034567f12eea4
Sander van Hooff <s.vanhooff@hotmail.com>
2025-10-30 15:48:52 +0100

    a

--- src/calculate.service.ts
+++ src/calculate.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CalculateService {
-  // Adds two numbers and returns the results
+  // Adds two numbers and returns the result
   sum(a: number, b: number): number {
     return a + b;
   }
